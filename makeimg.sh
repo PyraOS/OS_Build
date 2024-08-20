55b421fc08b4f1ce3058bece116d7cde7e0656eb
Wally4000 <catapultit@me.com>
2024-08-20 17:24:40 +1000

    Move back to Pyra certs

--- makeimg.sh
+++ makeimg.sh
@@ -142,12 +142,13 @@ fi
 # Pyra Packages Repo
 if [ "$OS_VERSION" -le 30 ]; then
 cat << EOF >> "${ROOTFS}"/etc/apt/sources.list.d/pyra-packages.list
-# deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://packages.pyra-handheld.com/ $OS/
-deb [arch=armhf trusted=yes] http://slater.au bookworm/
+deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://slater.au bookworm/
+# deb [arch=armhf trusted=yes] http://slater.au bookworm/
 EOF
 else
 cat << EOF >> "${ROOTFS}"/etc/apt/sources.list.d/pyra-packages.list
-deb [arch=armhf trusted=yes] http://slater.au bookworm/
+deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://slater.au bookworm/
+# deb [arch=armhf trusted=yes] http://slater.au bookworm/
 EOF
 fi 
 
