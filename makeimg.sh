60cb0a558385f078822e217b3d790d3741d73e59
Wally4000 <catapultit@me.com>
2024-08-20 17:38:02 +1000

    Gotta love naming hockey

--- makeimg.sh
+++ makeimg.sh
@@ -142,12 +142,12 @@ fi
 # Pyra Packages Repo
 if [ "$OS_VERSION" -le 30 ]; then
 cat << EOF >> "${ROOTFS}"/etc/apt/sources.list.d/pyra-packages.list
-deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://slater.au bookworm
+deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://slater.au bookworm/
 # deb [arch=armhf trusted=yes] http://slater.au bookworm/
 EOF
 else
 cat << EOF >> "${ROOTFS}"/etc/apt/sources.list.d/pyra-packages.list
-deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://slater.au bookworm
+deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://slater.au bookworm/
 # deb [arch=armhf trusted=yes] http://slater.au bookworm/
 EOF
 fi 
