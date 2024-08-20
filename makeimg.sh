597ec769d5819c80ffa0970317af16c24bdb8ec9
Wally4000 <catapultit@me.com>
2024-08-20 22:22:22 +1000

    Update gpg to use local gpg?

--- makeimg.sh
+++ makeimg.sh
@@ -117,8 +117,9 @@ fi
 
 
 #Fetch the Pyra key, convert it to gpg (see apt-key deprecation)
-curl -fsSL https://packages.pyra-handheld.com/pyra-public.pgp | sudo gpg --dearmor -o "${ROOTFS}"/usr/share/keyrings/pyra-public.gpg
 
+# curl -fsSL https://packages.pyra-handheld.com/pyra-public.pgp | sudo gpg --dearmor -o "${ROOTFS}"/usr/share/keyrings/pyra-public.gpg
+curl -fsSL https://slater.au/bookworm/Release.gpg -o "${ROOTFS}"/usr/share/keyrings/pyra-public.gpg
 echo "Setup Source Repos"
 
 cat << EOF > "${ROOTFS}"/etc/apt/sources.list
