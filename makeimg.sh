0bdce83905f8a3bf079394ce4e48d53f66bdfeaa
Wally4000 <catapultit@me.com>
2024-08-20 11:35:20 +1000

    See if this builds with my own repo

--- makeimg.sh
+++ makeimg.sh
@@ -24,6 +24,7 @@ IMAGESIZE="$2"
 
 #Bullseye and beyond supported, we need the OS version to automate detection for testing and SID. OS version beyond 30 will trigger testing and unstable.
 case $OS in 
+
 bullseye)
 OS_VERSION=11
 ;;
@@ -56,6 +57,7 @@ esac
 
 echo "OS Name: ${OS}"
 echo "OS Version: ${OS_VERSION}. Note for sid and testing it is 50"
+
 
 
 ARCHIVE_KEY="https://ftp-master.debian.org/keys/archive-key-$OS_VERSION.asc"
@@ -138,14 +140,13 @@ EOF
 fi 
 
 # Pyra Packages Repo
-# Unstable repo seems a bit stale, let's try bookworm instead for testing
 if [ "$OS_VERSION" -le 30 ]; then
 cat << EOF >> "${ROOTFS}"/etc/apt/sources.list.d/pyra-packages.list
 deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://packages.pyra-handheld.com/ $OS/
 EOF
 else
 cat << EOF >> "${ROOTFS}"/etc/apt/sources.list.d/pyra-packages.list
-deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://packages.pyra-handheld.com/ bookworm/
+deb [arch=armhf signed-by=/usr/share/keyrings/pyra-public.gpg] http://packages.pyra-handheld.com/ unstable/
 EOF
 fi 
 
